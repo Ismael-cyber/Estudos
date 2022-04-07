f44f66bb76d03c843c7a5ef212e482b5d8618133
Ismael Silva <ismaelsousasilva07@gmail.com>
2022-04-07 14:20:41 -0300

    fundamentos

---
+++ Fundamentos/Estrutura_Dados.js
@@ -0,0 +1,10 @@
+/* Estrutura de Dados, o que Seria ??
+ * Resposta : A Estrutura de Dados visa organizar e admistrar os seus dados 
+ * trazendo ordem a eles... 
+ *    
+ */
+
+
+
+
+
---
+++ Fundamentos/algoritmo_comeco.js
@@ -0,0 +1,14 @@
+/* Algoritmo seria o verbo, a ação do código a forma de se pensar como funcionaria seu programa
+ * Exemplificando cada passo para o sistema seguir e assim rodar seu código...    
+ */
+
+let notaAluno = 5;
+const media = 7;
+
+if (notaAluno >= media) {
+    console.log("O aluno foi Aprovado")
+}
+else (
+    console.log("Aluno foi Reprovado")
+)
+
